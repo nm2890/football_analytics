6484418f601c99a9e67e8ad9f8f500bc86db5e78
nm2890 <nm2890@github.com>
2023-07-21 09:08:46 +0200

    typo

--- odds/implied.py
+++ odds/implied.py
@@ -1,54 +1,44 @@
-def compute_implied_odds(odds,model = 'uniform') :
-  '''
-  Compute estimated implied odds for bookmakers odds
+import numpy as np
 
 
-  Parameters
-  --------
-  odds : list of decimal odds
-  model : model used
+def compute_implied_odds(odds, model='uniform'):
+
+    """
+    Compute estimated implied odds for bookmakers odds
 
 
-  Available models :
+    Input
+    --------
+    odds : array of decimal odds - size (nb_events,nb_odds)
+    model : model used
 
-  uniform : the margin is deduced in a uniform way for each odd
+    Available models :
+    uniform : the margin is deduced in a uniform way for each odd
+    proportional : the margin is proportional to odds, see
 
-  proportional : the margin is proportional to odds, see
+    Output
 
+    -------
+    array of odds_implied - size (nb_events,nb_odds)
 
+    TO DO
+    -------
+    Add more models !
+    Be able to deal with different odds format
+    check if all odds are in acceptable format
+    """
 
+    odds = np.array(odds)
 
-  Output
-  -------
-  odds_implied
+    inv_odds = 1 / odds
+    margin = np.sum(inv_odds, axis=1) - 1
+    nb_odds = odds.shape[1]
 
+    if model == 'uniform':
+      implied_odds = odds * np.reshape(1 + margin, (margin.shape[0], 1))
+    elif model == 'proportional':
+      implied_odds = nb_odds * odds / (nb_odds - np.reshape(margin, (margin.shape[0], 1)) * odds)
+    else :
+      raise Exception(f"Model {model} not implemented")
 
-
-  ## TO DO
-  # Add more models !
-  # Be able to deal with different odds format
-
-  '''
-
-  odds = np.array(odds)
-
-
-  # ## Check if odds are > 1 , else return a list of nan
-  # if not all(elem >= 1 for elem in odds) :
-  #   return([np.nan]*len(odds))
-
-
-  inv_odds = 1 / odds
-  margin = np.sum(inv_odds,axis=1) - 1
-
-  nb_odds = odds.shape[1]
-
-  if model == 'uniform' :
-    implied_odds = odds * np.reshape(1 + margin,(margin.shape[0],1))
-
-
-
-  elif model == 'proportional' :
-    implied_odds = nb_odds * odds / (nb_odds - np.reshape(margin,(margin.shape[0],1)) * odds)
-
-  return(implied_odds)
+    return (implied_odds)
